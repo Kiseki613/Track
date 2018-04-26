c52646e98ea9a46c5658b60b07106a7df03669e6
Kiseki <1292749008@qq.com>
2018-04-26 18:42:26 +0100

    5

--- Track/www/js/index.js
+++ Track/www/js/index.js
@@ -8,26 +8,33 @@
 $(document).on('pageshow', '#pageone', onLoad);
 
 var map;
+
+//the markers set by users
 var markers = [];
+
+//store markers'position set by users
 var markPosition = [];
+
+// the number of markers
 var m=0;
 
+
+
 function onLoad() {
-    
-    
     
    // alert("onLoad");
     console.log("onLoad");
     
-    
+    //initialize device
     onDeviceReady();
-   
+    
+    //initialize map
     initMap();
        
     
 }
 
-// Initialise device / HTML hooks
+// Initialize device / HTML hooks
 function onDeviceReady() {
     // Set map size dynamically
     $('#content').height(getRealContentHeight());
@@ -41,11 +48,15 @@ function onDeviceReady() {
     
     $('#start').on("click", function () {
         $("[data-role=panel]").panel("close");
+        
+        //change the text of footer to ON
         $('#trackText').text("ON");
         startTrack();
     });
     $('#stop').on("click", function () {
         $("[data-role=panel]").panel("close");
+        
+        //change the text of footer to OFF
         $('#trackText').text("OFF");
         stopTrack();
     });
@@ -79,6 +90,7 @@ function getRealContentHeight() {
     return content_height;
 }
 
+//check network connection, if no internet, create a toast message
 function checkConnection() {
     
    // alert("checkConnection");
@@ -86,7 +98,6 @@ function checkConnection() {
     console.log("checkConnection");
     
     var networkState = navigator.connection.type;
-    
     console.log(networkState);
     
     //alert(networkState);
@@ -148,12 +159,340 @@ function initMap() {
         styleSelector.addEventListener('change', function() {
           map.setOptions({styles: styles[styleSelector.value]});
         });
-
     
 }
 
 
 
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+     console.log("addMarker" )
+    var marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        markers[m] = marker;
+    
+    //store markers'position in the array markPosition 
+        markPosition[m] = location;
+        console.log("markPosition:"+markPosition[m]);
+    
+    //count the number of markers
+        m++;
+      }
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+        console.log("deleteMarkers")
+    
+        clearMarkers();
+        markers = [];
+        markPosition = [];
+        m = 0;
+      }
+
+ // Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+// a dialog of confirmation of deleting markers 
+function deleteMarkersDialog() {
+
+	navigator.notification.confirm(
+    	'Do you want to delete markers?',// message
+        deleteMarkersDismissed,         // callback
+        'Confirm',          // title
+        ['OK', 'Cancel']                  // buttons
+    );
+
+}
+
+
+//define the operation of delecting markers
+function deleteMarkersDismissed(buttonIndex) {
+	
+    //when users choose OK
+	if(buttonIndex==1) {
+        
+        //delete markers
+        deleteMarkers();
+        
+        //create a toast message to tell users markers have been deleted
+        new Toast({content: "Markers are deleted.", duration: 3000});
+    }
+    
+    //when users choose Cancel
+   	else if(buttonIndex==2) 
+        
+        //close the panel
+        $("[data-role=panel]").panel("close");
+
+}
+
+
+
+
+
+//Call this function when you want to get the current position
+function getPosition() {
+    
+    //checeking network connection before getting current position
+    checkConnection();  
+    
+    //determine the process of getting current position when it success and fail
+    navigator.geolocation.getCurrentPosition(successPosition, failPosition);
+    
+}
+
+
+//called when the position is successfully determined
+function successPosition(position) {
+    var long = position.coords.longitude;
+    var lat = position.coords.latitude;
+    var current = new google.maps.LatLng(lat, long);
+    setloc(current, 17);
+
+}
+
+//called when the position is failed determined
+function failPosition(err) {
+    alert('ERROR(' + err.code + '): ' + err.message);
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+
+//set location by location parameter and zoom
+function setloc(loc, zoom) {
+    map.setCenter(loc);
+    map.setZoom(zoom);
+}
+
+
+
+<!--Tracking-->
+
+//watchID for tracking
+var watchID;
+
+//the number of saved positions
+var i=0;
+
+//the arrey to put saved positions
+var trackPlanCoordinates=[];
+
+//the markers of tracking postion, and they are not set on map
+var markTrack;
+
+
+//the arrey to put track path
+var trackPath=[];
+
+//location options of tracking
+var locationOptions = { 
+	maximumAge: 10000, 
+	timeout: 6000, 
+	enableHighAccuracy: true
+};
+
+//Start track and set watchID
+function startTrack(){
+    
+    //call updateTrack when it success, call failTrack when it failed, and set location options
+    watchID = navigator.geolocation.watchPosition(updateTrack,failTrack,locationOptions);
+    
+}
+
+
+//update tracking
+function updateTrack(position){
+    
+    //check network before update tracking
+    checkConnection();
+   
+    console.log("tracking...");
+    console.log("a " + markPosition)
+    
+    //get current position' longitude and latitude
+    var lo = position.coords.longitude;
+    var la = position.coords.latitude;
+    
+    //set current location
+    var tracking = new google.maps.LatLng(la, lo);
+    setloc(tracking, 17);
+    
+    //create a marker for current location
+   /* markTrack = new google.maps.Marker({
+        position: tracking,
+        
+    });*/
+    
+
+    //store current location's latitude and longitude to an array
+    trackPlanCoordinates[i]={lat:la,lng:lo}
+    console.log(trackPlanCoordinates);
+    
+    //create tracking path by the saved location and store it to the trackPath array
+    trackPath[i] = new google.maps.Polyline({
+    path: trackPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 15
+  });
+    
+    //set the trackPath to map
+  trackPath[i].setMap(map);
+    
+  console.log(i);
+    
+    //count the number of stored position and track path
+  i++;
+    console.log("b " + markPosition[0])
+    
+    
+//when users reach the markers'positions they set, give them information
+//when the number of stored markers is bigger than 0
+if(markPosition.length!=0){  
+    
+   //absolute value of the stored markers' latitude
+   var a = [];
+    
+    //absolute value of the stored markers' longitude
+   var b = [];
+    
+    //absolute value of current latitude
+   var c = Math.abs(la);
+    
+    //absolute value of current longitude
+   var d = Math.abs(lo);
+    
+    //the square of a 
+   var e = [];
+    
+    //the squre of b
+   var f = [];
+    
+    //the distance between users' current position and stored markers
+   var g = [];
+    
+    //the number of the compared times
+   var x; 
+    
+    //comparing current potion with the stored markers' postions
+    for(x=0;x<markPosition.length;x++){
+         console.log("c " + markPosition[x]);
+        
+        //calculate the distance between users' current position and stored markers
+       a[x] = Math.abs(markPosition[x].lat());
+       b[x] = Math.abs(markPosition[x].lng());
+       e[x] = (a[x]-c)*(a[x]-c);
+       f[x] = (b[x]-d)*(b[x]-d);
+       g[x] = Math.sqrt(e[x]+f[x]); 
+       console.log("distance:"+g[x]);
+        
+        //if the distance is less than 0.005, create vibration and a toast message
+        if(g[x]<0.005){
+            
+            navigator.vibrate(6000);
+            new Toast({content: "You has reached the point you set.", duration: 3000});
+        // alert( "You has reached the point you set");
+           
+        } 
+    }  
+    
+    //initialize the parameters used for calculating distance
+    x=0;
+    a=[];
+    b=[];
+    e=[];
+    f=[];
+    g=[];
+ }
+    
+    
+    
+    
+}
+
+//Stop tracking
+function stopTrack() {
+    
+    //clear watchID and give a toast messsage
+	navigator.geolocation.clearWatch(watchID);
+    console.log("stop tracking");
+    new Toast({content: "Stop tracking", duration: 3000});
+}
+
+
+//clear all the stored track path from map
+ function clearTrack() {
+     
+    var n;
+     
+    for(n=0;n<i;n++){
+        
+    trackPath[n].setMap(null);
+        
+    }
+    
+  
+     //initialize the parameters of track path
+    trackPlanCoordinates=[];
+    i=0;
+     
+    console.log("track path was cleared.");
+   
+     
+ }
+
+// a dialog for comfirming clear track path
+function clearPathDialog() {
+
+	navigator.notification.confirm(
+    	'Do you want to clear the path?',// message
+        clearPathDismissed,         // callback
+        'Confirm',          // title
+        ['OK', 'Cancel']                  // buttons
+    );
+}
+
+
+//define the operation of clearing track path
+function clearPathDismissed(buttonIndex) {
+	
+    //when users choose OK
+	if(buttonIndex==1) {
+        //clear track path
+        clearTrack();
+        //create toast message
+        new Toast({content: "Track path is cleared.", duration: 3000});
+    }
+    
+    //when users choose Cancel, close the panel
+   	else if(buttonIndex==2) 
+        $("[data-role=panel]").panel("close");
+
+}
+
+//called if the position is not obtained correctly
+function failTrack(error) {
+    
+	console.log("The position is not obtained correctly.");
+	
+}
+
+
+
+
+//Map's styles
  var styles = {
         default: null,
         silver: [
@@ -449,277 +788,4 @@ function initMap() {
           }
         ]
       };
-
-
-
-
-
-
-// Adds a marker to the map and push to the array.
-function addMarker(location) {
-     console.log("addMarker" )
-    var marker = new google.maps.Marker({
-          position: location,
-          map: map
-        });
-        markers[m] = marker;
-       // markPosition[m] = { lat:location.lat(), lng:location.lng(), notified:no };
-        markPosition[m] = location;
-        console.log("markPosition:"+markPosition[m]);
-        m++;
-      }
-
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-        }
-      }
-
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-        console.log("deleteMarkers")
-    
-        clearMarkers();
-        markers = [];
-        markPosition = [];
-        m = 0;
-      }
-
- // Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-        setMapOnAll(null);
-      }
-
-
-function deleteMarkersDialog() {
-
-	navigator.notification.confirm(
-    	'Do you want to delete markers?',// message
-        deleteMarkersDismissed,         // callback
-        'Confirm',          // title
-        ['OK', 'Cancel']                  // buttons
-    );
-
-}
-
-function deleteMarkersDismissed(buttonIndex) {
-	
-	if(buttonIndex==1) {
-        deleteMarkers();
-        
-        new Toast({content: "Markers are deleted.", duration: 3000});
-    }
-   	else if(buttonIndex==2) 
-        $("[data-role=panel]").panel("close");
-
-}
-
-
-
-
-
-//Call this function when you want to get the current position
-function getPosition() {
-    
-    checkConnection();  
-    navigator.geolocation.getCurrentPosition(successPosition, failPosition);
-    
-}
-
-//called when the position is successfully determined
-function successPosition(position) {
-    var long = position.coords.longitude;
-    var lat = position.coords.latitude;
-    var current = new google.maps.LatLng(lat, long);
-    setloc(current, 17);
-
-}
-
-function failPosition(err) {
-    alert('ERROR(' + err.code + '): ' + err.message);
-    console.warn('ERROR(' + err.code + '): ' + err.message);
-}
-
-function setloc(loc, zoom) {
-    map.setCenter(loc);
-    map.setZoom(zoom);
-}
-
-
-<!--Tracking-->
-
-var watchID;
-
-//the number of saved positions
-var i=0;
-//the arrey to put saved positions
-var trackPlanCoordinates=[];
-var markTrack;
-//track path
-var trackPath=[];
-var locationOptions = { 
-	maximumAge: 10000, 
-	timeout: 6000, 
-	enableHighAccuracy: true
-};
-
-
-function startTrack(){
-    
-   
-    
-    watchID = navigator.geolocation.watchPosition(updateTrack,failTrack,locationOptions);
-    
-}
-
-
-function updateTrack(position){
-    checkConnection();
-   
-    console.log("tracking...");
-    
-    console.log("a " + markPosition)
-    
-    var lo = position.coords.longitude;
-    var la = position.coords.latitude;
-    var tracking = new google.maps.LatLng(la, lo);
-    setloc(tracking, 17);
-    markTrack = new google.maps.Marker({
-        position: tracking,
-    });
-    
-
-    
-    trackPlanCoordinates[i]={lat:la,lng:lo}
-
-       
-    console.log(trackPlanCoordinates);
-    trackPath[i] = new google.maps.Polyline({
-    path: trackPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 15
-  });
-  trackPath[i].setMap(map);
-    
-  console.log(i);
-  i++;
-   
-    
-    console.log("b " + markPosition[0])
-    
-if(markPosition.length!=0){  
-   var a = [];
-   var b = [];
-   var c = Math.abs(la);
-   var d = Math.abs(lo);
-   var e = [];
-   var f = [];
-   var g = [];
-   var x; 
-    for(x=0;x<markPosition.length;x++){
-        
-         console.log("c " + markPosition[x]);
-        
-        
-       a[x] = Math.abs(markPosition[x].lat());
-       b[x] = Math.abs(markPosition[x].lng());
-       e[x] = (a[x]-c)*(a[x]-c);
-       f[x] = (b[x]-d)*(b[x]-d);
-       g[x] = Math.sqrt(e[x]+f[x]); 
-        
-       console.log("distance:"+g[x]);
-        
-        if(g[x]<0.005){
-         createVibrate();
-        // alert( "You has reached the point you set");
-           
-        } 
-    }  
-    x=0;
-    a=[];
-    b=[];
-    e=[];
-    f=[];
-    g=[];
- }
-    
-    
-    
-    
-}
-
-
-function stopTrack() {
-	navigator.geolocation.clearWatch(watchID);
-    
-    console.log("stop tracking");
-    alert("stop tracking");
-}
-
-
- function clearTrack() {
-     
-    var n;
-     
-    for(n=0;n<i;n++){
-        
-    trackPath[n].setMap(null);
-        
-    }
-    
-  
-     
-    trackPlanCoordinates=[];
-    i=0;
-     
-    console.log("track path was cleared.");
-   
-     
- }
-
-
-function clearPathDialog() {
-
-
-      
-	navigator.notification.confirm(
-    	'Do you want to clear the path?',// message
-        clearPathDismissed,         // callback
-        'Confirm',          // title
-        ['OK', 'Cancel']                  // buttons
-    );
-
-}
-
-function clearPathDismissed(buttonIndex) {
-	
-	if(buttonIndex==1) {
-        clearTrack();
-        
-        new Toast({content: "Track path is cleared.", duration: 3000});
-    }
-   	else if(buttonIndex==2) 
-        $("[data-role=panel]").panel("close");
-
-}
-
-//called if the position is not obtained correctly
-function failTrack(error) {
-    
-    // checkConnection();
-
-	console.log("The position is not obtained correctly.");
-	
-}
-
-
-function createVibrate() {
-        		
-  navigator.vibrate(6000);
-  new Toast({content: "You has reached the point you set.", duration: 3000});
-    
-}
 
