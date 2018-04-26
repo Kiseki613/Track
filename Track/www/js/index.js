aa127147169e7a2afd4d08d8cf33e9b5a4cced68
Kiseki <1292749008@qq.com>
2018-04-26 17:02:51 +0100

    2

--- Track/www/js/index.js
+++ Track/www/js/index.js
@@ -137,7 +137,7 @@ function initMap() {
     
     
     // Add a style-selector control to the map.
-    /*    var styleControl = document.getElementById('style-selector-control');
+       var styleControl = document.getElementById('style-selector-control');
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(styleControl);
 
         // Set the map's style to the initial value of the selector.
@@ -147,14 +147,14 @@ function initMap() {
         // Apply new JSON when the user selects a different style.
         styleSelector.addEventListener('change', function() {
           map.setOptions({styles: styles[styleSelector.value]});
-        });*/
-    
+        });
+
     
 }
 
 
 
- /*var styles = {
+ var styles = {
         default: null,
         silver: [
           {
@@ -451,7 +451,6 @@ function initMap() {
       };
 
 
-*/
 
 
 
